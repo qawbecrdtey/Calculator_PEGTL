cc95484ae501e795b2db73f4d2acc726ca225dcb
qawbecrdtey <qawbecrdtey@naver.com>
2019-08-12 09:23:06 +0900

    Added version.hpp - Current version is 0.9.0

---
+++ Calculator/version.hpp
@@ -0,0 +1,10 @@
+#ifndef Calculator_version_hpp
+#define Calculator_version_hpp
+
+#define CALCULATOR_VERSION "0.9.0"
+
+#define CALCULATOR_VERSION_MAJOR 0
+#define CALCULATOR_VERSION_MINOR 9
+#define CALCULATOR_VERSION_PATCH 0
+
+#endif
